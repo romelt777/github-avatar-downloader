1b2e1cc8258465f258b9011ae607142878f76957
Romel Tan <romel.tan777@gmail.com>
2019-01-15 02:23:38 +0000

    added notes

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,23 @@
+
+
+//importing request package
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+// console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  //url we are using to download
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
 
+
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+//hard coding the call of function getRepoContributors
+getRepoContributors("jquery", "jquery", function(err, result){
   console.log("Errors:", err);
   console.log("Result:", result);
 });
