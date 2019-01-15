34b0ef83b17f1197d6573b962965fec55111b03c
Romel Tan <romel.tan777@gmail.com>
2019-01-15 03:12:21 +0000

    added headers, token import

--- download_avatars.js
+++ download_avatars.js
@@ -5,12 +5,26 @@ var request = require('request');
 
 // console.log('Welcome to the GitHub Avatar Downloader!');
 
+//importing github token
+var gToken = require('./secret.js');
+
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  //url we are using to download
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
 
+  var options {
+    //url we are using to download
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
 
-  request(url, function(err, res, body) {
+    //adding headers : allows more info to be passed between client and server
+    headers: {
+      'User-Agent': 'request'
+      'Authorization: token': gToken.GITHUB_TOKEN;
+
+    }
+
+  };
+
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 }
