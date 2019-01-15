577ec387b143301c6efc0b43c7fcf3c6c9550f1b
Romel Tan <romel.tan777@gmail.com>
2019-01-15 03:38:08 +0000

    fixed token authentication, also imported the token from another file

--- download_avatars.js
+++ download_avatars.js
@@ -11,14 +11,16 @@ var gToken = require('./secret.js');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  var options {
+  var options = {
     //url we are using to download
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
 
     //adding headers : allows more info to be passed between client and server
     headers: {
-      'User-Agent': 'request'
-      'Authorization: token': gToken.GITHUB_TOKEN;
+      // 'User-Agent': 'request'
+      //'GITHUB_TOKEN': gToken.GITHUB_TOKEN
+      // 'Authorization': 'Basic ' + gToken
+      'User-Agent': gToken
 
     }
 
@@ -27,7 +29,7 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request(options, function(err, res, body) {
     cb(err, body);
   });
-}
+};
 
 
 //hard coding the call of function getRepoContributors
